555130fb27e1c684117b1fc3c093945e9a75e9e6
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2022-11-14 19:07:16 -0500

    Update ToKThEmployee.sql

--- coinflip/onlineTest/ToKThEmployee.sql
+++ coinflip/onlineTest/ToKThEmployee.sql
@@ -10,3 +10,19 @@ select id, name,
        salary
 from (SELECT id, name, divisionID, managerId, salary FROM maintable_C4MZO ORDER BY Salary DESC LIMIT 3) emp
 order by salary LIMIT 1;
+
+select emp.id,
+       emp.name,
+       (select division.name from cb_companydivisions division where division.id = emp.divisionID) as divisionName,
+       managerId,
+       case
+           when (select manager.name from cb_companymanager manager where manager.id = emp.managerId) is null
+               then emp.managerId
+           else (select manager.name
+                 from cb_companymanager manager
+                 where manager.id = emp.managerId)
+           end                                                                                     as "Manager Name/ID",
+       salary
+from (SELECT id, name, divisionID, managerId, salary FROM maintable_c4mzo ORDER BY salary DESC LIMIT 3) emp
+order by salary
+LIMIT 1;
